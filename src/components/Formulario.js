585245b59271c9891cd0a3ac9111f50f04c576fc
Ernesto Ferreyra <ferreyraernesto29@gmail.com>
2021-03-22 18:29:43 -0300

    agregamos formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,20 @@
+
+
+
+const Formulario = () => {
+    return ( 
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name
+
+            </div>
+
+
+        </form>
+
+     );
+}
+ 
+export default Formulario;
